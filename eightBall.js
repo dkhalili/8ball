dbb3482cf18199f72add811448a4800f0e38baa4
Daniel Khalili <dkhalili93@gmail.com>
2015-07-28 14:21:24 -0400

    begin

---
+++ eightBall.js
@@ -0,0 +1,40 @@
+var net = require("net");
+var port = 2001;
+var fs = require("fs");
+
+
+var server = net.createServer(function(client) {
+	console.log("connected");
+	client.setEncoding("utf8");
+
+	client.write("Hi I'm the magic 8 ball!\nAsk a question and I'll give you a response\nMake sure you use ? or else I might just ignore you...\n\n")
+
+	//
+	var ask = function ask() {
+		fs.readFile("eightBall.json", "utf8", function(err, data) {
+			if (err) {
+				console.log(err);
+			}
+			else {
+				var parsed = JSON.parse(data);
+				var randQ = parsed[Math.floor(Math.random()*parsed.length - 1)]; 
+				client.write(randQ);
+			}
+		})
+	
+	}
+	//
+
+	client.on("data", function(clientData) {
+		ask();
+	})
+
+
+
+})
+
+
+
+server.listen(port, function(){
+	console.log("listening to " + port);
+})
